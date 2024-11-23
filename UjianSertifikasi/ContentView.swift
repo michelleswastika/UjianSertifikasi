0c17da4e91ac4537daa8826341ba844486ebd466
michelleswastika <94586964+michelleswastika@users.noreply.github.com>
2024-11-23 15:57:11 +0700

    Initial Commit

---
+++ UjianSertifikasi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UjianSertifikasi
+//
+//  Created by Michelle Swastika on 23/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UjianSertifikasi/UjianSertifikasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UjianSertifikasiApp.swift
+//  UjianSertifikasi
+//
+//  Created by Michelle Swastika on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UjianSertifikasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
